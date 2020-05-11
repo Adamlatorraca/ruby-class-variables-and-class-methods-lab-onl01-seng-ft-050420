04aa477287be0c5cd28c03575742ba25169b8380
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-11 19:35:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre_count << genre
-    end
-    genre_count.length
+      if genre_count[genre]
+        
   end
 end
